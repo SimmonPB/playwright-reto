bc9ce2f2b32805dc54853372288aaa4e177e7b66
SIMON <simmon32aa@gmail.com>
2026-09-19 17:30:34 -0500

    reto goback terminado y playwright con 2 minutos para ejecutar pruebas

---
+++ tests/retogoback.spec.ts
@@ -0,0 +1,64 @@
+import { test, expect } from '@playwright/test';
+
+test('check left options', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+    await page.getByPlaceholder('Password').fill('admin123');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
+
+    const leftmenuitems = page.getByLabel('sidepanel').getByRole('listitem')
+    const currentmenuitems = await leftmenuitems.count()
+
+    console.log('Current menu items: ', currentmenuitems)
+
+
+    const currentmenuitemsnames: string[] = []
+
+    for (let i = 0; i < currentmenuitems; i++) {
+
+        const menuitem = await leftmenuitems.nth(i).innerText()
+        currentmenuitemsnames.push(menuitem)
+    }
+
+    console.log(currentmenuitemsnames)
+
+    const expectedmenuitemsnames = ['Admin', 'PIM', 'Leave', 'Time', 'Recruitment', 'My Info', 'Performance', 'Dashboard', 'Directory', 'Maintenance', 'Claim', 'Buzz']
+
+    expect(currentmenuitemsnames).toEqual(expectedmenuitemsnames)
+
+
+
+
+})
+
+test('navigate through the left panel', async ({ page }) => {
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+    await page.getByPlaceholder('Password').fill('admin123');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
+
+    const leftmenuitems = page.getByLabel('sidepanel').getByRole('listitem')
+    const currentmenuitems = await leftmenuitems.count()
+
+    for (let i = 0; i < currentmenuitems; i++) {
+        const menuitem = leftmenuitems.nth(i)
+        const menuitemname = await menuitem.innerText()
+        console.log('current menu item: ', menuitemname)
+
+        await menuitem.click()
+        await page.waitForLoadState('networkidle')
+        await page.waitForTimeout(5)
+
+        if (menuitemname === 'Maintenance') {
+            await page.goBack()
+            await page.waitForLoadState('networkidle')
+            await page.waitForTimeout(100)
+        }
+    }
+
+})

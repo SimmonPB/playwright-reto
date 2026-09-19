fa074afef00ea90b11262f30f026127143ba0e35
SIMON <simmon32aa@gmail.com>
2026-09-19 16:31:55 -0500

    extraer daotos de employeename

---
+++ tests/employeename.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+
+test('Get all the usernames registered', async ({ page }) => {
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+    await page.getByPlaceholder('Password').fill('admin123');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
+    await page.getByRole('link', { name: 'Admin' }).click()
+
+    await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('User Management').click()
+    await page.getByRole('menuitem', { name: 'Users' }).click()
+
+    const table = page.getByRole('table').filter({ has: page.getByText('Employee Name') })
+    const rows = table.getByRole('row')
+    const employeeNames: string[] = []
+
+    await rows.nth(1).waitFor()
+    const rowCount = await rows.count()
+
+    for (let i = 1; i < rowCount; i++) {
+
+        const cell = rows.nth(i).getByRole('cell').nth(3)
+        const employeeName = await cell.textContent()
+
+        if (employeeName) {
+            employeeNames.push(employeeName)
+        }
+    }
+
+    console.log(employeeNames)
+})

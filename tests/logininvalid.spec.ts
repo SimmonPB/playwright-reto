95f69f24b692e9d33bb70158d47a2d8be9770ae1
SIMON <simmon32aa@gmail.com>
2026-09-19 15:17:53 -0500

    HRM con credenciales invalidas

---
+++ tests/logininvalid.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('login with invalid credentials', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+  await page.getByPlaceholder('Password').fill('admin1234');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await expect(page.getByText('Invalid credentials')).toBeVisible();
+});
